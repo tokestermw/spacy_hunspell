0b8102d91d50e0c8b96a8943d0ed36c5ef7566f9
Motoki Wu <tokestermw@gmail.com>
2017-11-27 09:45:53 -0800

    minimize test and add environmental variable

--- test.py
+++ test.py
@@ -2,24 +2,15 @@ from __future__ import unicode_literals
 
 import os
 
-LIB_DIR = '/usr/share/hunspell'
+DEFAULT_PATH = os.environ.get('DEFAULT_PATH', 'linux')
+
 
 if __name__ == '__main__':
-    # -- default hunspell test
-    from hunspell import HunSpell
-    hobj = HunSpell(
-        os.path.join(LIB_DIR, 'en_US.dic'),
-        os.path.join(LIB_DIR, 'en_US.aff'))
-
-    assert hobj.spell('spookie') == False
-    assert hobj.suggest('spookie')[:5] == \
-        ['spookier', 'spookiness', 'spook', 'cookie', 'bookie']
-
     # -- spacy pipeline test
     import spacy
     from spacy_hunspell import spaCyHunSpell
     nlp = spacy.load('en_core_web_sm')
-    hunspell = spaCyHunSpell(nlp, 'linux')
+    hunspell = spaCyHunSpell(nlp, DEFAULT_PATH)
     nlp.add_pipe(hunspell)
 
     doc = nlp('I can haz cheezeburger.')
